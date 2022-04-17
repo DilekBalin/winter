fb12dc16362162914a4c8dc38115faf85b70ef2e
DilekBalin <dilekkara1231@gmail.com>
2022-04-17 15:48:09 +0100

    LinkedList eklendi

--- src/day46_iterator_collections/C05_LinkedList.java
+++ src/day46_iterator_collections/C05_LinkedList.java
@@ -4,15 +4,16 @@ import java.util.LinkedList;
 
 public class C05_LinkedList {
     public static void main(String[] args) {
-        // hem List, hem de Queue'nun child class'idir
+        // LinkedList hem List, hem de Queue'nun child class'idir
+        //list. deyince llist de olan tum methodlar gelir ve ekstradan baska method lar da vardir
         LinkedList<Integer> list = new LinkedList<>();
         list.add(5);
         list.add(10); // add method'u List'den geldigi icin hep sona ekler
-        list.addFirst(11); // addFirst deque'den gelir
-        list.addLast(12); // addLast deque'den gelir
+        list.addFirst(11); // addFirst de Queue'den gelir
+        list.addLast(12); // addLast de "Queue"den gelir
         System.out.println(list);
         list.add(2,25);
-        System.out.println(list);
+        System.out.println(list); //araya da eleman ekleyebiliriz
     }
 }
 
